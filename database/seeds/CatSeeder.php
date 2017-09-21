defff223a24fef74017600fd4363e9d0fa4c2253
= <=>
2017-09-21 14:10:44 +0700

    remove unused file

--- database/seeds/CatSeeder.php
+++ database/seeds/CatSeeder.php
@@ -12,23 +12,6 @@ class CatSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('category')->insert([
-        	'category'=>'blackberry'
-        	]);
-        DB::table('category')->insert([
-        	'category'=>'yahoo'
-        ]);
-        DB::table('category')->insert([
-        	'category'=>'nokia'
-        	]);
-        DB::table('category')->insert([
-        	'category'=>'travekoka'
-        ]);
-        DB::table('category')->insert([
-        	'category'=>'gojek'
-        	]);
-        DB::table('category')->insert([
-        	'category'=>'yahoo'
-        ]);
+       
     }
 }
